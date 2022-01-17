ff9a235f84ced429a864fa1568b05794f8d746d4
VanRar <vitalikf7@gmail.com>
2022-01-17 21:03:57 +0300

    Added methods from spring_rest project

--- src/main/java/org/vanrar/spring/springboot/springboot/controller/MyRestController.java
+++ src/main/java/org/vanrar/spring/springboot/springboot/controller/MyRestController.java
@@ -22,34 +22,34 @@ public class MyRestController {
         return allEmployees;
     }
 
-//    @GetMapping("/employees/{id}")
-//    public Employee getEmployee(@PathVariable int id) {
-//
-//        Employee employee = employeeService.getEmployee(id);
-//
-//        return employee;
-//    }
-//
-//    @PostMapping("/employees")
-//    public Employee addNewEmployee(@RequestBody Employee employee) {
-//
-//        employeeService.saveEmployee(employee);
-//        return employee;
-//    }
-//    @PutMapping("/employees")
-//    public Employee updateEmployee(@RequestBody Employee employee) {
-//
-//        employeeService.saveEmployee(employee);//тк с id, то будет уже обновление
-//        return employee;
-//    }
-//
-//    @DeleteMapping("/employees/{id}")
-//    public  String deleteEmployee(@PathVariable int id){
-//
-//
-//        employeeService.deleteEmployee(id);
-//        return "Employee with id " + id  + " was deleted";
-//
-//    }
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+
+        Employee employee = employeeService.getEmployee(id);
+
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+
+        employeeService.saveEmployee(employee);//тк с id, то будет уже обновление
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public  String deleteEmployee(@PathVariable int id){
+
+
+        employeeService.deleteEmployee(id);
+        return "Employee with id " + id  + " was deleted";
+
+    }
 
 }
